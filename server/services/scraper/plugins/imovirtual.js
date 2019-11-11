9a9832132e7088f875998b8f6f282c9556acd68f
Diogo Marques <diogo@fontainhas.com>
2019-11-11 22:55:45 +0000

    folder structure

--- server/services/scraper/plugins/imovirtual.js
+++ server/services/scraper/plugins/imovirtual.js
@@ -3,7 +3,7 @@ const cheerio = require("cheerio");
 
 const url = 'https://www.imovirtual.com/arrendar/apartamento/?search%5Bfilter_float_m%3Ato%5D=50&search%5Bdescription%5D=1&search%5Bregion_id%5D=11&nrAdsPerPage=72';
 
-axios(url)
+module.exports.imovirtual = axios(url)
 .then(response => {
   const html = response.data;
   const $ = cheerio.load(html);

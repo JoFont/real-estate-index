c0a586ff51738b74be3d827cbdde722300b3f5a9
Diogo Marques <diogo@fontainhas.com>
2019-11-12 15:28:12 +0000

    need to crawl multiple pages

--- server/services/scraper/plugins/olx.js
+++ server/services/scraper/plugins/olx.js
@@ -3,7 +3,7 @@ const cheerio = require("cheerio");
 const puppeteer = require("puppeteer");
 
 const url = 'https://www.olx.pt/imoveis/?search%5Bdescription%5D=1';
-
+let page;
 
 puppeteer
   .launch({ignoreDefaultArgs: ['--disable-extensions']})
@@ -14,32 +14,40 @@ puppeteer
     });
   }).then(html => {
     const $ = cheerio.load(html);
+    const pages = $(".pager.rel.clr").find("a").each((i, el) => {
+      if($(el).data("cy") === "page-link-last") {
+        page = Number($(el).children().text());
+      }
+    });
+
+    console.log(page);
+    // const pages = $("page-link-last")
     const articles = $("#offers_table tr.wrap");
 
     let results = [];
 
-    articles.each((i, el) => {
-      const features = $(el).data('features');
+    // articles.each((i, el) => {
+    //   const features = $(el).data('features');
       
-      if(features && features["cat_l3_name"]) {
-        const title = $(el).find(".title-cell > div > h3 > a > strong").text();
-        const imgUrl = $(el).find("td > a.thumb > img").attr("src");
-        const propertyType = features["cat_l3_name"] ? features["cat_l3_name"].split("-")[0].slice(0, -1) : null;
-        const listingType = features["cat_l3_name"].split("-")[1] === "venda" ? "buy" : "rent";
+    //   if(features && features["cat_l3_name"]) {
+    //     const title = $(el).find(".title-cell > div > h3 > a > strong").text();
+    //     const imgUrl = $(el).find("td > a.thumb > img").attr("src");
+    //     const propertyType = features["cat_l3_name"] ? features["cat_l3_name"].split("-")[0].slice(0, -1) : null;
+    //     const listingType = features["cat_l3_name"].split("-")[1] === "venda" ? "buy" : "rent";
     
-        results.push({
-          title,
-          price: features["ad_price"],
-          currency: features["price_currency"],
-          city: features["city_name"],
-          region: features["region_name"],
-          imgUrl,
-          propertyType,
-          listingType,
-          // features
-        });
-      }
-    });
+    //     results.push({
+    //       title,
+    //       price: features["ad_price"],
+    //       currency: features["price_currency"],
+    //       city: features["city_name"],
+    //       region: features["region_name"],
+    //       imgUrl,
+    //       propertyType,
+    //       listingType,
+    //       features
+    //     });
+    //   }
+    // });
   
     console.log(results);
 
